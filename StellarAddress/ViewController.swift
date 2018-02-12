648b9ff7f5e11884da778a2a29f1a189270c8edd
kuyawa <haxapp@gmail.com>
2018-02-12 15:28:40 -0400

    Initial Commit

---
+++ StellarAddress/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StellarAddress
+//
+//  Created by Laptop on 2/12/18.
+//  Copyright © 2018 Armonia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
